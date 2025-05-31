466e11cbeeb4e553c185cb6f9e617a6065a0527b
BaibaiPangpang <baibaipangpang.bbpp@gmail.com>
2025-05-30 23:08:04 -0700

    made data also a hyper parameter for training and logging.

--- augment.py
+++ augment.py
@@ -5,11 +5,13 @@ class CalligraphyAugmentations(nn.Module):
     def __init__(self):
         super().__init__()
         self.augment = nn.Sequential(
+            K.ColorJitter(brightness=0.1, contrast=0.1, p=0.5),             # exposure variation
+            K.RandomGrayscale(p=0.2),                                       # ensure grayscale
             K.RandomRotation(degrees=3.0, p=0.5),
-            K.RandomResizedCrop((512, 512), scale=(0.95, 1.05), ratio=(1.0, 1.0), p=0.5),
+            # K.RandomResizedCrop((512, 512), scale=(0.95, 1.05), ratio=(1.0, 1.0), p=0.5),
             K.RandomGaussianBlur((3, 3), sigma=(0.1, 1.0), p=0.5),
-            K.RandomElasticTransform(kernel_size=(63, 63), sigma=(4.0, 6.0), alpha=(1.0, 2.0), p=0.3),
-            K.RandomErasing(scale=(0.01, 0.03), ratio=(0.3, 3.3), same_on_batch=False, p=0.2),
+            # K.RandomElasticTransform(kernel_size=(63, 63), sigma=(4.0, 6.0), alpha=(1.0, 2.0), p=0.3),
+            # K.RandomErasing(scale=(0.01, 0.03), ratio=(0.3, 3.3), same_on_batch=False, p=0.2),
         )
 
     def forward(self, x):
